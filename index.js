703476edd90748f00c8c123ee754a2576a93bc1d
Your Name <you@example.com>
2019-06-16 01:21:56 +0700

    Query parameter

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ var port = 3000;
 app.set('view engine', 'pug');
 app.set('views', './views');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+var users = [
+    { id : 1, name : 'Hoang An' },
+    { id : 2, name : 'My Duyen' }
+]
+
 app.get('/', function(req, res) {
     res.render('index', {
         name : 'AAA'
@@ -14,11 +22,27 @@ app.get('/', function(req, res) {
 
 app.get('/users', function(req, res) {
     res.render('users/index', {
-        users : [
-            { id : 1, name : 'A' },
-            { id : 2, name : 'B' }
-        ]
+        users : users
     });
+});
+
+app.get('/users/search', function(req, res) {
+    var q = req.query.q;
+    var matchedUser = users.filter(function(user) {
+        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
+    });
+    res.render('users/index', {
+        users : matchedUser
+    });
+});
+
+app.get('/users/create', function(req, res) {
+    res.render('users/create');
+});
+
+app.post('/users/create', function(req, res) {
+    users.push(req.body);
+    res.redirect("/users");
 });
 
 app.listen(port, function() {
