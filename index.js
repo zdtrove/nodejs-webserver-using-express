67580996f11f65ac327c2772d35801dbb2ec0068
Your Name <you@example.com>
2019-06-19 00:42:59 +0700

    Nodejs express

--- index.js
+++ index.js
@@ -1,17 +1,9 @@
 var express = require('express');
 var app = express();
 
+var userRoute = require('./routes/user.route');
+
 var port = 3000;
-
-var low = require('lowdb');
-var shortid = require('shortid');
-
-var FileSync = require('lowdb/adapters/FileSync');
-var adapter = new FileSync('db.json');
-
-db = low(adapter);
-
-db.defaults({ users : [] }).write();
 
 app.set('view engine', 'pug');
 app.set('views', './views');
@@ -25,39 +17,7 @@ app.get('/', function(req, res) {
     });
 });
 
-app.get('/users', function(req, res) {
-    res.render('users/index', {
-        users : db.get('users').value()
-    });
-});
-
-app.get('/users/search', function(req, res) {
-    var q = req.query.q;
-    var matchedUser = users.filter(function(user) {
-        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-    });
-    res.render('users/index', {
-        users : matchedUser
-    });
-});
-
-app.get('/users/create', function(req, res) {
-    res.render('users/create');
-});
-
-app.get('/users/:id', function(req, res) {
-    var id = req.params.id;
-    var user = db.get('users').find({ id : id }).value();
-    res.render('users/view', {
-        user : user
-    });
-});
-
-app.post('/users/create', function(req, res) {
-    req.body.id = shortid.generate();
-    db.get('users').push(req.body).write();
-    res.redirect("/users");
-});
+app.use('/users', userRoute);
 
 app.listen(port, function() {
     console.log('Server listening on port ' + port);
---
+++ routes/user.route.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+var controller = require('../controllers/user.controller');
+
+router.get('/', controller.index);
+
+router.get('/search', controller.search);
+
+router.get('/create', controller.create);
+
+router.get('/:id', controller.get);
+
+router.post('/create', controller.postCreate);
+
+module.exports = router;
